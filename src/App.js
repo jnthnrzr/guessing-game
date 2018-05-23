f040edfb26ea866553d66aabf9d8b16b8ad6d072
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-05-23 00:13:06 -0400

    Add wireframe for numpad.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App container">
+
+                <div className="row header">
+                    <div className="display">display</div>
+                </div>
+                <div className="row">
+                    <button className="button num-button">1</button>
+                    <button className="button num-button">2</button>
+                    <button className="button num-button">3</button>
+                </div>
+                <div className="row">
+                    <button className="button num-button">4</button>
+                    <button className="button num-button">5</button>
+                    <button className="button num-button">6</button>
+                </div>
+                <div className="row">
+                    <button className="button num-button">7</button>
+                    <button className="button num-button">8</button>
+                    <button className="button num-button">9</button>
+                </div>
+                <div className="row">
+                    <button className="button clear">&#x232b;</button>
+                    <button className="button num-button">0</button>
+                    <button className="button enter">&#x2713;</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
