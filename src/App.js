73c782cec7e6fd0c5917d3c8628842a9ddcc691e
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-05-23 00:48:02 -0400

    Show display value and add or remove numbers to display by clicking the buttons.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,57 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+            lastValue: null,
+        };
+        this.onNumBtnClick = this.onNumBtnClick.bind(this);
+        this.onClrBtnClick = this.onClrBtnClick.bind(this);
+
+    }
+
+    onNumBtnClick(e) {
+        const number = Number(e.target.innerHTML);
+        const { value } = this.state;
+        const newValue = value ? (value * 10 + number) : number;
+        this.setState({value: newValue});
+    }
+
+    onClrBtnClick() {
+        const { value } = this.state;
+        if (value) {
+            const newValue = Math.floor(value / 10);
+            this.setState({value: newValue});
+        }
+    }
+
     render() {
+        const { value } = this.state;
         return (
             <div className="App container">
 
                 <div className="row header">
-                    <div className="display">display</div>
+                    <div className="display">{Number(value)}</div>
                 </div>
                 <div className="row">
-                    <button className="button num-button">1</button>
-                    <button className="button num-button">2</button>
-                    <button className="button num-button">3</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>1</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>2</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>3</button>
                 </div>
                 <div className="row">
-                    <button className="button num-button">4</button>
-                    <button className="button num-button">5</button>
-                    <button className="button num-button">6</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>4</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>5</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>6</button>
                 </div>
                 <div className="row">
-                    <button className="button num-button">7</button>
-                    <button className="button num-button">8</button>
-                    <button className="button num-button">9</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>7</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>8</button>
+                    <button className="button num-button" onClick={this.onNumBtnClick}>9</button>
                 </div>
                 <div className="row">
-                    <button className="button clear">&#x232b;</button>
+                    <button className="button clear" onClick={this.onClrBtnClick}>&#x232b;</button>
                     <button className="button num-button">0</button>
                     <button className="button enter">&#x2713;</button>
                 </div>
