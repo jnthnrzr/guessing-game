a54631b4a587187d1c9fe3fc4486f82e4aac81eb
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-06-03 13:33:03 -0400

    Separate NumPad as a component.

---
+++ src/Components/NumPad.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './NumPad.css';
+import NumButton from './NumButton';
+
+function NumPad(props) {
+    // Extract buttonHandlers from props.
+    const { buttonHandlers } = props;
+    const {onNumBtnClick, onEnterBtnClick, onClrBtnClick} = buttonHandlers;
+
+    // Setup the NumPad button rows.
+    const buttonTemplate = (number) => (
+        <NumButton onNumBtnClick={onNumBtnClick} key={number} value={number} />
+    );
+
+    const firstRowBtns = [1, 2, 3].map(buttonTemplate);
+    const secondRowBtns = [4, 5, 6].map(buttonTemplate);
+    const thirdRowBtns = [7, 8, 9].map(buttonTemplate);
+    const lastRowBtns = (
+        <div>
+            <button className="button clear" onClick={onClrBtnClick}>&#x232b;</button>
+            <NumButton onNumBtnClick={onNumBtnClick} value={0}/>
+            <button className="button enter" onClick={onEnterBtnClick}>&#x2713;</button>
+        </div>
+    );
+
+    // Construct the NumPad div.
+    return (
+        <div className="numpad">
+            <div className="row">{firstRowBtns}</div>
+            <div className="row">{secondRowBtns}</div>
+            <div className="row">{thirdRowBtns}</div>
+            <div className="row">{lastRowBtns}</div>
+        </div>
+    );
+}
+
+export default NumPad;
