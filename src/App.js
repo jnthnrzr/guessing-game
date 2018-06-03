66bbe44216c79f16185775926774a1808a5578c8
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-06-03 17:14:28 -0400

    Implement modular design with components in App.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,86 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
+import Hint from './Components/Hint';
+import Header from './Components/Header';
+import NumPad from './Components/NumPad';
 
-class App extends Component {
+const isCorrect = (answer, correctAnswer) => (answer === correctAnswer);
+
+const getRandomInt = (min = 0, max = 100) => {
+    // The maximum is exclusive and the minimum is inclusive.
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: null,
-            lastValue: null,
+            iteration: 0,
+            haveWon: false,
+            target: getRandomInt(),
+            guess: 0,
+            lastGuess: 0,
         };
         this.onNumBtnClick = this.onNumBtnClick.bind(this);
         this.onClrBtnClick = this.onClrBtnClick.bind(this);
-
+        this.onEnterBtnClick = this.onEnterBtnClick.bind(this);
     }
 
-    onNumBtnClick(e) {
-        const number = Number(e.target.innerHTML);
-        const { value } = this.state;
-        const newValue = value ? (value * 10 + number) : number;
-        this.setState({value: newValue});
+    onNumBtnClick(number) {
+        const { guess } = this.state;
+        const newGuess = guess ? (guess * 10 + number) : number;
+        this.setState({ guess: newGuess });
     }
 
     onClrBtnClick() {
-        const { value } = this.state;
-        if (value) {
-            const newValue = Math.floor(value / 10);
-            this.setState({value: newValue});
+        const { guess } = this.state;
+        if (guess) {
+            const newValue = Math.floor(guess / 10);
+            this.setState({ guess: newValue });
         }
     }
 
-    render() {
-        const { value } = this.state;
-        return (
-            <div className="App container">
+    onEnterBtnClick() {
+        const { guess, target, iteration } = this.state;
 
-                <div className="row header">
-                    <div className="display">{Number(value)}</div>
+        // Check if current guess matches the target.
+        const correct = isCorrect(guess, target);
+        console.log(correct.toString());
+
+        this.setState({
+            iteration: iteration + 1,
+            lastGuess: guess,
+            guess: 0,
+            haveWon: correct,
+        });
+    }
+
+    // TODO delete target div.
+    render() {
+        const { guess, lastGuess, target, haveWon } = this.state;
+
+        const buttonHandlers = {
+            onNumBtnClick: this.onNumBtnClick,
+            onEnterBtnClick: this.onEnterBtnClick,
+            onClrBtnClick: this.onClrBtnClick
+        };
+
+        return (
+            <div className="App">
+
+                <div className="output">
+                    <Header haveWon={haveWon} />
+                    <Hint guess={lastGuess} target={target} />
+
+                    <div className="row center">Target: {target}</div>
                 </div>
-                <div className="row">
-                    <button className="button num-button" onClick={this.onNumBtnClick}>1</button>
-                    <button className="button num-button" onClick={this.onNumBtnClick}>2</button>
-                    <button className="button num-button" onClick={this.onNumBtnClick}>3</button>
+
+                <div className="input">
+                    <NumPad guess={guess} buttonHandlers={buttonHandlers} />
                 </div>
-                <div className="row">
-                    <button className="button num-button" onClick={this.onNumBtnClick}>4</button>
-                    <button className="button num-button" onClick={this.onNumBtnClick}>5</button>
-                    <button className="button num-button" onClick={this.onNumBtnClick}>6</button>
-                </div>
-                <div className="row">
-                    <button className="button num-button" onClick={this.onNumBtnClick}>7</button>
-                    <button className="button num-button" onClick={this.onNumBtnClick}>8</button>
-                    <button className="button num-button" onClick={this.onNumBtnClick}>9</button>
-                </div>
-                <div className="row">
-                    <button className="button clear" onClick={this.onClrBtnClick}>&#x232b;</button>
-                    <button className="button num-button">0</button>
-                    <button className="button enter">&#x2713;</button>
-                </div>
+
             </div>
         );
     }
