c824f49d850157aadacd2df03f654d4447365032
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-06-05 19:15:03 -0400

    Use correct wording for tries.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,15 +4,16 @@ import './Header.css';
 function Header(props) {
     const { haveWon, iteration, target } = props;
     const min = 0, max = 100, count = 10 - iteration;
+    const tryOrTries = count > 1 ? "tries" : "try";
 
     const instruction = (
         <div>
-            Guess the number<br/> between <br/> {min} and {max} <br/> in <br/> {count} tries
+            Guess the number<br/> between <br/> {min} and {max} <br/> in <br/> {count} {tryOrTries}
         </div>
     );
 
     const stillInTheGame = iteration < 10;
-    const message = haveWon ? "You've won!" : instruction;
+    const message = haveWon ? <span>You've won!<br/>The number was {target}.</span> : instruction;
     const ranOutOfTries = (
         <div>
             You ran out of tries.<br/> The number was {target}.
